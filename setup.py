b6885ea4035ed0ec88cc69e2c9cb9b3ac24f546c
neutrinomuon <neutrinomuon@gmail.com>
2023-06-07 14:47:50 +0100

    Changed Structure of PyIntegral

--- setup.py
+++ setup.py
@@ -4,12 +4,15 @@ from numpy.distutils.core import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-ext1 = Extension(  name='pyintegralall.flib',
+with open("version.txt", "r") as vh:
+    version_description = vh.read()
+    
+ext1 = Extension(  name='PyIntegral.flib',
                    sources=['src/fortran/DataTypes.f90','src/fortran/LINinterpol.f90','src/fortran/GaussLegendreQuadrature.f90','src/fortran/IntegralALL.f90'],
                  )
     
-setup( name='pyintegralall',
-       version='0.0.9',
+setup( name='PyIntegral',
+       version=version_description,
        ext_modules=[ ext1 ],
        extra_compile_args=['-O3'],
        description='Integrate arrays, functions numerically using different methods in Python. Original Fortran 2003+ legacy routines date back to 2003-2004. Read the LICENSE.txt file.',
@@ -17,15 +20,15 @@ setup( name='pyintegralall',
        long_description_content_type="text/markdown",
        author='Jean Gomes',
        author_email='antineutrinomuon@gmail.com',
-       url='https://github.com/neutrinomuon/pyintegralall',
+       url='https://github.com/neutrinomuon/PyIntegral',
        install_requires=[ 'numpy','matplotlib' ],
        classifiers=[
            "Programming Language :: Python :: 3",
            "Programming Language :: Fortran",
            "Operating System :: OS Independent",
                    ],
-       package_dir={"pyintegralall": "src/python"},
-       packages=['pyintegralall'],
+       package_dir={"PyIntegral": "src/python"},
+       packages=['PyIntegral'],
        data_files=[('', ['version.txt'])],
       )
     
