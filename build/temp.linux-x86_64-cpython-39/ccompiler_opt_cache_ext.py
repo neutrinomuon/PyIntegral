efebff25a7e19e7d79b18fc002399dcf5de12f79
neutrinomuon <neutrinomuon@gmail.com>
2023-12-27 13:28:16 +0000

    Modified README, including in ZENODO

--- build/temp.linux-x86_64-cpython-39/ccompiler_opt_cache_ext.py
+++ build/temp.linux-x86_64-cpython-39/ccompiler_opt_cache_ext.py
@@ -126,7 +126,7 @@ data = \
                                              '-msse4.2'],
               "('feature_flags', 'SSSE3')": ['-msse', '-msse2', '-msse3',
                                              '-mssse3'],
-              "('feature_flags', {'SSE3', 'SSE2', 'SSE'})": ['-msse', '-msse2',
+              "('feature_flags', {'SSE3', 'SSE', 'SSE2'})": ['-msse', '-msse2',
                                                              '-msse3'],
               "('feature_is_supported', 'AVX', 'force_flags', 'macros', None, [])": True,
               "('feature_is_supported', 'AVX2', 'force_flags', 'macros', None, [])": True,
